f84979833c08e76a643070d9fa89b6e455ca1959
Senilliani <bren.senilliani@gmail.com>
2025-12-02 17:05:00 +0100

    use v-if

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <h1 :class="{ highlight: myCondition, red: true }">HOLA</h1>
+  <h2 v-if="myCondition">Si la condición se cumple</h2>
+  <h3 v-else-if="condition2">Esta es la condición 2</h3>
+  <h3 v-else>No se cumplen las demás</h3>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-let myCondition = true
+let myCondition = ''
+let condition2 = false
 </script>
 
 <style scoped>
