7207692b33152e3b5a3dad5f8276bb8178972079
Senilliani <bren.senilliani@gmail.com>
2025-12-02 16:20:29 +0100

    use event

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <HeaderComponent></HeaderComponent>
   <h1>HOLA</h1>
+  <h2 @click="addOne">{{ number }}</h2>
 </template>
 
 <script setup lang="ts">
-import HeaderComponent from './components/HeaderComponent.vue';
+import { ref } from 'vue';
+import HeaderComponent from './components/HeaderComponent.vue'
+
+const number = ref(0);
+
+const addOne = () => {
+  number.value++
+}
+
 </script>
 
 <style scoped></style>
