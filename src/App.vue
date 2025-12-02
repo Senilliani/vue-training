01aa2d3624dba08c1630dcfe4f5e5cbcee660cac
Senilliani <bren.senilliani@gmail.com>
2025-12-02 19:49:17 +0100

    use props

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,16 @@
 <template>
-  <div>
-    <p v-for="(movie, i) in movies" :key="movie">
-      {{ i + 1 }} - {{ movie }}
-    </p>
-  </div>
-  <ul>
-    <li v-for="animal in animals" :key="animal.id">
-      {{ animal.name }}
-      <img :src="animal.img" alt="" width="200px">
-    </li>
-  </ul>
+  <ChildComponent :message="text" :person="person"></ChildComponent>
+  <ChildComponent :message="text"></ChildComponent>
 </template>
 
 <script setup>
-const movies = ["Lord", "Star", "Dune"]
-const animals = [
-  {
-    id: 1,
-    name: "dog",
-    img: "https://cdn.pixabay.com/photo/2025/11/28/15/29/zebras-9983175_1280.jpg",
-  },
-  {
-    id: 2,
-    name: "cat",
-    img: "https://cdn.pixabay.com/photo/2025/11/28/15/29/zebras-9983175_1280.jpg",
-  },
-  {
-    id: 3,
-    name: "pig",
-    img: "https://cdn.pixabay.com/photo/2025/11/28/15/29/zebras-9983175_1280.jpg",
-  },
-];
+import ChildComponent from './components/ChildComponent.vue';
+
+const text = "desde el padre"
+const person = {
+  sex: "mujer",
+  age: 30
+}
 </script>
 
 <style scoped></style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <h2>
+        Componente hijo
+    </h2>
+
+    <p>{{ message }}</p>
+    <p v-if="person">{{ person.sex }} - {{ person.age }}</p>
+</template>
+
+<script setup>
+defineProps({
+    message: String,
+    person: {
+        sex: String,
+        age: Number
+    }
+})
+</script>
+
+<style scoped></style>
