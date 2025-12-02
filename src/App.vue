13ccfefcd0aba3af77bc1dc4540f0066aea39378
Senilliani <bren.senilliani@gmail.com>
2025-12-02 17:24:09 +0100

    use v-for with array

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,13 @@
 <template>
-  <h1 :class="{ highlight: myCondition, red: true }">HOLA</h1>
-  <h2 v-if="myCondition">Si la condición se cumple</h2>
-  <h3 v-else-if="condition2">Esta es la condición 2</h3>
-  <h3 v-else>No se cumplen las demás</h3>
+  <div>
+    <p v-for="(movie, i) in movies" :key="movie">
+      {{ i + 1 }} - {{ movie }}
+    </p>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-let myCondition = ''
-let condition2 = false
+const movies = ["Lord", "Star", "Dune"]
 </script>
 
-<style scoped>
-.highlight {
-  background-color: black;
-  color: yellow;
-  text-align: center;
-}
-
-.red {
-  color: red;
-}
-</style>
+<style scoped></style>
