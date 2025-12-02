9eca8d8b0df9854d81a6d1c56105619e82fcc5a3
Senilliani <bren.senilliani@gmail.com>
2025-12-02 15:52:49 +0100

    create components

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Home Page</a></li>
+                <li><a href="#">Blog de notas</a></li>
+                <li><a href="#">Contactos</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+</script>
+
+<style scoped></style>
