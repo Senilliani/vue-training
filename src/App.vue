4168a98aacc29a4286b17badc4fb7cbba7cbbe0c
Senilliani <bren.senilliani@gmail.com>
2025-12-02 19:14:12 +0100

    use v-for with array of objects

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,33 @@
       {{ i + 1 }} - {{ movie }}
     </p>
   </div>
+  <ul>
+    <li v-for="animal in animals" :key="animal.id">
+      {{ animal.name }}
+      <img :src="animal.img" alt="" width="200px">
+    </li>
+  </ul>
 </template>
 
 <script setup>
 const movies = ["Lord", "Star", "Dune"]
+const animals = [
+  {
+    id: 1,
+    name: "dog",
+    img: "https://cdn.pixabay.com/photo/2025/11/28/15/29/zebras-9983175_1280.jpg",
+  },
+  {
+    id: 2,
+    name: "cat",
+    img: "https://cdn.pixabay.com/photo/2025/11/28/15/29/zebras-9983175_1280.jpg",
+  },
+  {
+    id: 3,
+    name: "pig",
+    img: "https://cdn.pixabay.com/photo/2025/11/28/15/29/zebras-9983175_1280.jpg",
+  },
+];
 </script>
 
 <style scoped></style>
