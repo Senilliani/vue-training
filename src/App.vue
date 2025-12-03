1f06b956a4861c7efdab86c12953cfe65ef24007
Senilliani <bren.senilliani@gmail.com>
2025-12-03 13:26:43 +0100

    use generic slots

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <h2>Hola {{ displayName }} desde el padre</h2>
-  <ChildComponent @login="setName"></ChildComponent>
+  <h2>Componente padre</h2>
+  <p>Pasa la información a través de slot al hijo</p>
+  <ChildComponent>
+    <h3>
+      desde el padre: al agregar contenido dentro de la etiqueta del hijo
+    </h3>
+  </ChildComponent>
+  <ChildComponent>
+    <h3>desde el padre: Lorem, ipsum dolor.</h3>
+  </ChildComponent>
+  <ChildComponent></ChildComponent>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 import ChildComponent from './components/ChildComponent.vue';
 
-const displayName = ref("")
-
-const setName = (e) => {
-  displayName.value = e;
-}
 </script>
 
 <style scoped></style>
--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -1,27 +1,25 @@
 <template>
     <div class="child">
-        <h2>
-            Componente hijo
-        </h2>
-        <p>Nombre de usuario {{ userName }}</p>
-        <button @click="login" style="margin-bottom: 20px;">Iniciar Sesión</button>
+        <h2>Componente hijo</h2>
+        <p>
+            recibe un bloque de código HTML y tiene la etiqueta SLOT
+        </p>
+        <slot>
+            <h3>(Contenido por defecto que lo escribe el hijo dentro de la etiqueta slot para cuando el padre no envía
+                nada)</h3>
+        </slot>
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-const emit = defineEmits(["login"])
-
-const login = () => {
-    emit("login", userName.value)
-}
-
-const userName = ref("Brenda")
 </script>
 
 <style scoped>
 .child {
     border: 5px solid black;
     text-align: center;
+    padding: 10px;
+    margin-top: 10px;
 }
 </style>
