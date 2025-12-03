ee46f578153ac5f765318444f17a4aab5a47f87b
Senilliani <bren.senilliani@gmail.com>
2025-12-03 17:55:28 +0100

    use form

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,41 @@
 <template>
-  <h2>Watch</h2>
-  <h3>Hola {{ userName }}</h3>
-  <button @click="setName">Cambio de usuario</button>
+  <h1>Formularios</h1>
+  <form @submit.prevent="handleSubmit" class="form">
+    <label for="title">Título</label>
+    <input v-model="movie.title" type="text" id="title">
+
+    <label for="duration">Duración</label>
+    <input v-model="movie.duration" type="text" id="duration">
+
+    <label for="director">Directora</label>
+    <input v-model="movie.director" type="text" id="director">
+
+    <button>Enviar</button>
+  </form>
 </template>
 
 <script setup>
-import { computed, ref, watchEffect } from 'vue';
+import { reactive } from 'vue';
 
-const userName = ref("Mar") // esta es la variable que se quiere vigilar
-
-const setName = computed(() => {
-  return userName.value = "María"
+const movie = reactive({
+  title: "",
+  duration: 0,
+  director: ""
 })
 
-// diferencia con el watch es que no se necesita pasar la variable como primer parámetro y que siempre es immediate
-watchEffect(() => {
-  console.log("variable que estoy vigilando:", userName.value)
-})
+const handleSubmit = () => {
+  if (!movie.title) {
+    alert("Debe tener al menos el título")
+    return
+  }
+  console.log("datos de la película: ", movie)
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+</style>
